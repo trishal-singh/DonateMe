72ab7d8fcfacbdc859315d278b139680720be9ed
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-04-27 16:15:08 +0530

    added donation model

---
+++ backend/models/donation.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const donationSchema = mongoose.Schema({
+   amount:{
+    type : Number,
+    required : true
+   },
+   user_id:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true
+   },
+   fund_id:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "funds",
+    required: true
+   }
+   
+})
+module.exports = mongoose.model('donations' , donationSchema )
