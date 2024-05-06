69641bb057e6d6ef5e2853d212ce5d1d33196be9
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-05-06 16:43:44 +0530

    Added DonateCard container

--- frontend/src/components/DonateCard.jsx
+++ frontend/src/components/DonateCard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const DonateCard = ({ title, image, target }) => {
+const DonateCard = ({ title, image, target, raised }) => {
   return (
-    <div className="border-2 border-lime-500 flex flex-col h-60 w-80 justify-evenly items-center rounded-2xl">
-      <img src={image} height="100px" width="150px" />
-      <div>{title}</div>
-      <div>Target : {target}</div>
-      <div>Button</div>
+    <div className="border-2 border-lime-500 flex flex-col h-72 w-80 justify-evenly items-center rounded-2xl">
+      <img src={image} className="h-32 w-44" />
+      <div className="font-mono font-bold">{title}</div>
+      <div className="font-mono italic">
+        Raised : {raised}/{target}
+      </div>
+      <button className="border-2 border-lime-500 rounded w-36 h-10  font-bold font-mono hover:bg-lime-500 hover:text-white">
+        {" "}
+        Know More
+      </button>
     </div>
   );
 };
--- frontend/src/pages/Donate.jsx
+++ frontend/src/pages/Donate.jsx
@@ -9,8 +9,6 @@ const Donate = () => {
     try {
       const result = await axiosClient.get("/fund");
       setFunds(result.data.data);
-      console.log(result.data.data);
-      console.log(funds);
     } catch (e) {
       toast.error(e.response.data.message, { position: "top-center" });
     }
@@ -21,9 +19,18 @@ const Donate = () => {
   return (
     <>
       <Navbar />
-      {funds.map((fund) => (
-        <div>{fund.title}</div>
-      ))}
+      <div className="grid gap-6 grid-cols-4 ml-10">
+        {funds.map((fund) => {
+          return (
+            <DonateCard
+              title={fund.title}
+              image={fund.image}
+              raised={fund.current}
+              target={fund.target}
+            />
+          );
+        })}
+      </div>
       <ToastContainer />
     </>
   );
