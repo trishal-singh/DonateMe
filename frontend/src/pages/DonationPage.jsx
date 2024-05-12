5353a003f2c949e785c130ef37817262474c75ff
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-05-12 15:55:16 +0530

    Added backend logic for DonationPage

--- frontend/src/pages/DonationPage.jsx
+++ frontend/src/pages/DonationPage.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axiosClient from "../services/axiosClient";
+import { ToastContainer, toast } from "react-toastify";
 const DonationPage = () => {
   const { id } = useParams();
-  return <div>DonationPage {id}</div>;
+  const [fund, setFund] = useState({});
+  const getFundById = async () => {
+    try {
+      const result = await axiosClient.get(`/fund/${id}`);
+      setFund(result.data.data);
+      console.log(fund);
+    } catch (e) {
+      toast.error(e.response.data.message, { position: "top-center" });
+    }
+  };
+  useEffect(() => {
+    getFundById();
+  }, []);
+  return (
+    <>
+      <div>{fund.title}</div>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default DonationPage;
