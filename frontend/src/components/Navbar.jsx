60006962a0e4f2e443ec9e29d6cf324988383776
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-05-16 16:44:25 +0530

    Added Logout Functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { FaUser } from "react-icons/fa";
 import { useNavigate } from "react-router";
+import Cookies from "js-cookie";
+import { ToastContainer, toast } from "react-toastify";
 const Navbar = () => {
   const navigate = useNavigate();
+  const handleAuth = () => {
+    if (Cookies.get("token") === undefined) navigate("/login");
+    else {
+      Cookies.remove("token");
+      toast.success("Logged Out", { position: "top-center" });
+    }
+  };
   return (
     <div className="flex items-center justify-between h-24  ">
       <div
@@ -37,12 +46,21 @@ const Navbar = () => {
           My Donations
         </div>
       </div>
-      <div
-        className="mr-5  text-2xl text-lime-500 hover:cursor-pointer hover:scale-125"
-        onClick={() => navigate("/profile")}
-      >
-        <FaUser />
+      <div className="flex items-center justify-evenly w-1/4 text-xl text-lime-500 ">
+        <button
+          className="border-2 border-lime-500 rounded w-36 h-10  font-bold font-mono hover:bg-lime-500 hover:text-white"
+          onClick={handleAuth}
+        >
+          LOGIN/LOGOUT
+        </button>
+        <div
+          className="mr-5  text-2xl text-lime-500 hover:cursor-pointer hover:scale-125"
+          onClick={() => navigate("/profile")}
+        >
+          <FaUser />
+        </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
