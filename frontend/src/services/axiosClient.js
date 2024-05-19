149d6eeae69d0562e9aba4d72408ddaf4e170ec1
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-05-19 08:51:50 +0530

    Updated backend url

--- frontend/src/services/axiosClient.js
+++ frontend/src/services/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://donateme-backend.onrender.com/",
 });
 export default axiosClient;
