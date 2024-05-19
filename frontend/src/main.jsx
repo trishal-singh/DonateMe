7f6868dad2d038ef624887c15493126144507994
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-05-19 08:37:19 +0530

    disable react dev tools for production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import axiosClient from "../services/axiosClient";
 import Cookies from "js-cookie";
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 import { useNavigate } from "react-router";
 const Profile = () => {
   const [name, setName] = useState("");
